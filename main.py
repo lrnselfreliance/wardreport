88c66e26956d5e15d3923a3d9076472a8af6d2f2
Roland <roland@learningselfreliance.com>
2022-01-23 12:01:59 -0700

    Moving data processing into report class.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python3
 import asyncio
-import dataclasses
 import json
 import os
 from datetime import datetime
@@ -39,58 +38,100 @@ def get_data(*a, **kw):
         return json.load(fh)
 
 
-@dataclasses.dataclass
 class MemberReport:
-    members: int = 0
-    non_members: int = 0
 
-    households: int = 0
+    def __init__(self, member_list, callings, recommend_status):
+        self.member_list = member_list
+        self.callings = callings
+        self.recommend_status = recommend_status
+        self.recommend_finder = recommend_finder_maker(recommend_status)
+        self.calling_finder = calling_finder_maker(self.callings)
 
-    adults: int = 0
-    non_adults: int = 0
+        self.data = dict()
+        self.process_data()
 
-    brethren: int = 0
-    brethren_with_callings: int = 0
-    brethren_melchizedek: int = 0
-    brethren_aaronic: int = 0
-    brethren_unordained: int = 0
+    def process_data(self):
+        """
+        Calculate and summarize all member information.
+        """
+        members, non_members = member_splitter(self.member_list)
 
-    sisters: int = 0
-    sisters_with_callings: int = 0
+        predicates = [
+            lambda i: i['age'] < 12,
+            lambda i: 12 <= i['age'] <= 17,
+            lambda i: i['age'] > 17,
+        ]
+        primary, young_people, adults = multi_partition(predicates, self.member_list)
+        young_men, young_women = male_splitter(young_people)
 
-    young_men: int = 0
-    young_men_aaronic: int = 0
-    young_men_unordained: int = 0
+        brethren, sisters = male_splitter(adults)
+        brethren_with_callings, brethren_no_calling = calling_splitter(self.calling_finder, brethren)
+        sisters_with_callings, sisters_no_calling = calling_splitter(self.calling_finder, sisters)
 
-    young_women: int = 0
+        bpg = priesthood_grouper(brethren)
+        b_high_priest, b_elder, b_priest, b_teacher, b_deacon, b_unordained = bpg['HIGH_PRIEST'], \
+                                                                              bpg['ELDER'], \
+                                                                              bpg['PRIEST'], \
+                                                                              bpg['TEACHER'], \
+                                                                              bpg['DEACON'], \
+                                                                              bpg['UNORDAINED']
+        ypg = priesthood_grouper(young_men)
+        y_priest, y_teacher, y_deacon, y_unordained = ypg['PRIEST'], ypg['TEACHER'], ypg['DEACON'], ypg['UNORDAINED']
 
-    primary: int = 0
-    age_0_to_2: int = 0
-    age_3_to_7: int = 0
-    age_8_plus: int = 0
+        predicates = [
+            lambda i: i['age'] <= 2,
+            lambda i: 2 < i['age'] <= 7,
+            lambda i: i['age'] >= 8,
+        ]
+        age_0_to_2, age_3_to_7, age_8_plus = multi_partition(predicates, primary)
 
-    single_18: int = 0
-    single_31: int = 0
-    single_46: int = 0
+        single_18, single_31, single_46 = singles_by_age(members)
 
-    endowed: int = 0
-    not_endowed: int = 0
+        endowed, not_endowed = endowed_splitter(self.recommend_finder, adults)
+        recommend_groups = recommend_status_grouper(self.recommend_status)
 
-    recommend_active: int = 0
-    recommend_expiring_next_month: int = 0
-    recommend_expiring_this_month: int = 0
+        self.data['members'] = len(members)
+        self.data['non_members'] = len(non_members)
+        self.data['households'] = len(household_grouper(self.member_list))
+        self.data['primary'] = len(primary)
+        self.data['adults'] = len(adults)
+        self.data['young_women'] = len(young_women)
+        self.data['sisters'] = len(sisters)
+        self.data['sisters_with_callings'] = len(sisters_with_callings)
+        self.data['sisters_no_calling'] = len(sisters_no_calling)
+        self.data['young_men'] = len(young_men)
+        self.data['young_men_aaronic'] = len(y_priest) + len(y_teacher) + len(y_deacon)
+        self.data['young_men_unordained'] = len(y_unordained)
+        self.data['brethren'] = len(brethren)
+        self.data['brethren_with_callings'] = len(brethren_with_callings)
+        self.data['brethren_no_calling'] = len(brethren_no_calling)
+        self.data['brethren_melchizedek'] = len(b_elder) + len(b_high_priest)
+        self.data['brethren_aaronic'] = len(b_priest) + len(b_teacher) + len(b_deacon)
+        self.data['brethren_unordained'] = len(b_unordained)
+        self.data['age_0_to_2'] = len(age_0_to_2)
+        self.data['age_3_to_7'] = len(age_3_to_7)
+        self.data['age_8_plus'] = len(age_8_plus)
+        self.data['single_18'] = len(single_18)
+        self.data['single_31'] = len(single_31)
+        self.data['single_46'] = len(single_46)
+        self.data['endowed'] = len(endowed)
+        self.data['not_endowed'] = len(not_endowed)
+        self.data['current_recommend'] = len(recommend_groups['ACTIVE']) + \
+                                         len(recommend_groups['EXPIRING_NEXT_MONTH']) + \
+                                         len(recommend_groups['EXPIRING_THIS_MONTH'])
+        self.data['expired_recommend'] = len(recommend_groups['CANCELED']) + \
+                                         len(recommend_groups['EXPIRED_LESS_THAN_1_MONTH']) + \
+                                         len(recommend_groups['EXPIRED_LESS_THAN_3_MONTHS']) + \
+                                         len(recommend_groups['EXPIRED_OVER_3_MONTHS'])
+        self.data.update({f'recommend_{k.lower()}': len(v) for k, v in recommend_groups.items()})
 
-    recommend_canceled: int = 0
-    recommend_expired_less_than_1_month: int = 0
-    recommend_expired_less_than_3_months: int = 0
-    recommend_expired_over_3_months: int = 0
-    recommend_lost_or_stolen: int = 0
+    def __getattr__(self, item):
+        return self.data[item]
 
     def print_report(self):
-        current_recommend = self.recommend_active + self.recommend_expiring_this_month + \
-                            self.recommend_expiring_next_month
-        expired_recommend = self.recommend_expired_over_3_months + self.recommend_expired_less_than_1_month + \
-                            self.recommend_expired_less_than_3_months + self.recommend_canceled
+        """
+        Print a text report of my data to stdout.
+        """
         print(f'''Report Date: {datetime.now().date()}
 
 Total Members: {self.members}
@@ -140,88 +181,12 @@ Temple
 Adults:
 \tEndowed: {self.endowed} ({percent_str(self.endowed, self.adults)})
 \tNot Endowed: {self.not_endowed} ({percent_str(self.not_endowed, self.adults)})
-\tCurrent Recommend: {current_recommend} ({percent_str(current_recommend, self.endowed)})
-\tExpired Recommend: {expired_recommend} ({percent_str(expired_recommend, self.endowed)})
+\tCurrent Recommend: {self.current_recommend} ({percent_str(self.current_recommend, self.endowed)})
+\tExpired Recommend: {self.expired_recommend} ({percent_str(self.expired_recommend, self.endowed)})
 \tRecommend Expiring this month: {self.recommend_expiring_this_month}
 \tRecommend Expiring next month: {self.recommend_expiring_next_month}
 \tRecommend Lost or Stolen: {self.recommend_lost_or_stolen}
 ''')
-
-
-def summarize_members(data: dict) -> MemberReport:
-    all_members = data['member_list']
-
-    calling_finder = calling_finder_maker(data['callings'])
-    recommend_status = data['recommend_status']
-    recommend_finder = recommend_finder_maker(recommend_status)
-
-    members, non_members = member_splitter(all_members)
-    households = household_grouper(all_members)
-
-    predicates = [
-        lambda i: i['age'] < 12,
-        lambda i: 12 <= i['age'] <= 17,
-        lambda i: i['age'] > 17,
-    ]
-    primary, young_people, adults = multi_partition(predicates, all_members)
-    young_men, young_women = male_splitter(young_people)
-
-    brethren, sisters = male_splitter(adults)
-    brethren_with_calling, brethren_no_calling = calling_splitter(calling_finder, brethren)
-    sisters_with_calling, sisters_no_calling = calling_splitter(calling_finder, sisters)
-
-    bpg = priesthood_grouper(brethren)
-    b_high_priest, b_elder, b_priest, b_teacher, b_deacon, b_unordained = bpg['HIGH_PRIEST'], \
-                                                                          bpg['ELDER'], \
-                                                                          bpg['PRIEST'], \
-                                                                          bpg['TEACHER'], \
-                                                                          bpg['DEACON'], \
-                                                                          bpg['UNORDAINED']
-    ypg = priesthood_grouper(young_men)
-    y_priest, y_teacher, y_deacon, y_unordained = ypg['PRIEST'], ypg['TEACHER'], ypg['DEACON'], ypg['UNORDAINED']
-
-    predicates = [
-        lambda i: i['age'] <= 2,
-        lambda i: 2 < i['age'] <= 7,
-        lambda i: i['age'] >= 8,
-    ]
-    age_0_to_2, age_3_to_7, age_8_plus = multi_partition(predicates, primary)
-
-    single_18, single_31, single_46 = singles_by_age(members)
-
-    endowed, not_endowed = endowed_splitter(recommend_finder, adults)
-    recommend_groups = recommend_status_grouper(recommend_status)
-
-    report = MemberReport()
-    report.members = len(members)
-    report.non_members = len(non_members)
-    report.households = len(households)
-    report.adults = len(adults)
-    report.non_adults = len(primary) + len(young_people)
-    report.brethren = len(brethren)
-    report.brethren_with_callings = len(brethren_with_calling)
-    report.brethren_melchizedek = len(brethren_no_calling)
-    report.brethren_aaronic = len(b_priest) + len(b_teacher) + len(b_deacon)
-    report.brethren_unordained = len(b_unordained)
-    report.sisters = len(sisters)
-    report.sisters_with_callings = len(sisters_with_calling)
-    report.young_men = len(young_men)
-    report.young_men_aaronic = len(y_priest) + len(y_teacher) + len(y_deacon)
-    report.young_men_unordained = len(y_unordained)
-    report.young_women = len(young_women)
-    report.primary = len(primary)
-    report.age_0_to_2 = len(age_0_to_2)
-    report.age_3_to_7 = len(age_3_to_7)
-    report.age_8_plus = len(age_8_plus)
-    report.single_18 = len(single_18)
-    report.single_31 = len(single_31)
-    report.single_46 = len(single_46)
-    report.endowed = len(endowed)
-    report.not_endowed = len(not_endowed)
-    for name, group in recommend_groups.items():
-        setattr(report, f'recommend_{name.lower()}', len(group))
-
-    return report
 
 
 async def main(username: str, password: str, number: str):
@@ -230,7 +195,7 @@ async def main(username: str, password: str, number: str):
     # with open('data.json', 'wt') as fh:
     #     json.dump(data, fh, indent=2)
 
-    member_report = summarize_members(data)
+    member_report = MemberReport(data['member_list'], data['callings'], data['recommend_status'])
     member_report.print_report()
 
 
